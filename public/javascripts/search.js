322418df8dfeb917cb168225d516137b907af827
Long <loandrew@amazon.com>
2018-10-23 14:47:10 -0700

    fixed bug in the front page search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -6,14 +6,13 @@ $( document ).ready(function() {
             data: {'searchText':$('#search-text').val()},
             success: function(data){
                 var html = template(data)
-                console.log(html)
-                $('#search-results').replaceWith(html)
+                document.getElementById('search-results').innerHTML = html
             }
         });
     }
     var source   = document.getElementById("search-template").innerHTML;
     var template = Handlebars.compile(source);
-    $('#search-text').on('keyup paste', $.throttle( 250, search()))
+    $('#search-text').on('keyup paste', $.throttle( 250, search));
 
     search();
 });
